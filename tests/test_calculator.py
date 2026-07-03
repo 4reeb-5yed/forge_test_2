8e8e7b37619cf6114996e917d261b50232f6ab3c
Forge <forge@forge-runtime.dev>
2026-07-03 20:11:25 +0000

    forge: 5

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,13 @@
 import unittest
-from calculator import add
+from calculator import subtract
 
 class TestCalculator(unittest.TestCase):
-    def test_add_positive(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(10, 20), 30)
+    def test_subtract_positive(self):
+        self.assertEqual(subtract(10, 3), 7)
 
-    def test_add_negative(self):
-        self.assertEqual(add(-2, -3), -5)
-        self.assertEqual(add(-10, -20), -30)
+    def test_subtract_negative(self):
+        self.assertEqual(subtract(-5, -2), -3)
 
-    def test_add_zero(self):
-        self.assertEqual(add(0, 0), 0)
-        self.assertEqual(add(5, 0), 5)
-        self.assertEqual(add(0, -5), -5)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_subtract_zero(self):
+        self.assertEqual(subtract(0, 0), 0)
+        self.assertEqual(subtract(5, 5), 0)
